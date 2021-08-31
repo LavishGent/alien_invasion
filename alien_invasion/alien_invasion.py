004facda99a096c87a37d11f394e0d5468bfa2df
Darrell Green <darrellgreendavis@yahoo.com>
2021-08-30 20:39:23 -0400

    Progress made
    Ship moves laterally and stops at screens edge
    Bullets are fired straight ahead with the press of the spacebar

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall calss to manage game assests and behavior"""
@@ -14,9 +15,19 @@ class AlienInvasion:
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        """
+        To make game window fullscreen replace above with below
+        -------------------------------------------------------
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        -------------------------------------------------------
+        """
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -24,6 +35,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 
@@ -34,22 +46,42 @@ class AlienInvasion:
                 sys.exit()
             # Checking for other events, each keypress is registered as a KEYDOWN event.
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        # If right arrow is pressed ship moves to the right
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        # If left arrow is pressed ship moves to the left
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        # If q is pressed game will quit
+        elif event.key == pygame.K_q:
+            sys.exit()
+        # If spacebar is pressed a bullet is fired
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
                 elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         # Redaraw the screen during each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
